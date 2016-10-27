3af44cef391581ea6a4a96bec1034889956ba5b2
ArkadiuszStelmach <Erni2021@gmail.com>
2016-10-27 11:48:16 +0200

    Delete config.inc.php

--- config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-Define ('DB_user','root');
-Define ('DB_password','');
-Define ('DB_host','localhost');
-Define ('DB_name','miejsca_warte_odwiedzenia');
-
-$dbc=mysqli_connect (DB_host,DB_user,DB_password,DB_name);
-?>
